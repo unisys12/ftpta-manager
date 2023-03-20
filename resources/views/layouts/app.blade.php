891351587b6bdd6e964785f6208ed0d64d1535b0
Phillip Jackson <unisys12@gmail.com>
2023-03-20 07:40:00 -0500

    update spacing of settings nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <header class="bg-white shadow text-center">
         <div class="flex justify-between space-x-7 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             {{ $header }}
-            <nav class="space-x-4">
+            <nav class="space-x-6">
                 <span class="text-xl font-bold">Settings: </span>
                 <Link href="{{ route('service.index') }}">Services</Link>
                 <Link href="{{ route('service_category.index') }}">Categories</Link>
